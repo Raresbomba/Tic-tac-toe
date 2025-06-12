350057bf8ec3def08b1e006cfdf3b9aca044a5ca
Rares Chirita <zzrareszz@gmail.com>
2025-06-12 23:09:20 +0300

    Changed SDL.dll for x32 to x64

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 
-int main() {
+int SDL_main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Tic Tac Toe",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, 0);
